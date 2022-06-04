3bed1bf7912735089e6ebb0c3b661f1007d817a1
SeveralCamper <mikha.alkhimovich@mail.ru>
2022-06-04 15:41:05 +0700

    develop: finish CocktailSort

--- src/DSA/main.cpp
+++ src/DSA/main.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <SelectSort.h>
 #include <BubbleSort.h>
+#include <ShakerSort.h>
 
 int main () {   	
     int MassLength = 100;
@@ -8,7 +9,8 @@ int main () {
     FillRand(Massive, MassLength,50,500);
     PrintMas(Massive, MassLength);
     std::cout << std::endl;
-    //SelectSort(Massive, MassLength);
-    BubbleSort(Massive, MassLength);
+    // SelectSort(Massive, MassLength);
+    // BubbleSort(Massive, MassLength);
+    ShakerSort(Massive, MassLength);
     PrintMas(Massive, MassLength);
 }
