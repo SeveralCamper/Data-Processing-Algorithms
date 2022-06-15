0b1c60bf67024bfb37e548254843090b388a8bb3
SeveralCamper <mikha.alkhimovich@mail.ru>
2022-06-15 14:55:23 +0700

    develop: finish HeapSort

--- src/DSA/main.cpp
+++ src/DSA/main.cpp
@@ -4,6 +4,7 @@
 #include <ShakerSort.h>
 #include <InsertSort.h>
 #include <ShellSort.h>
+#include <HeapSort.h>
 
 int main () {   	
     int MassLength = 100;
@@ -11,10 +12,16 @@ int main () {
     FillRand(Massive, MassLength,50,500);
     PrintMas(Massive, MassLength);
     std::cout << std::endl;
+    // Методы квадратичной трудоемкости
     // SelectSort(Massive, MassLength);
     // BubbleSort(Massive, MassLength);
     // ShakerSort(Massive, MassLength);
-    InsertSort(Massive, MassLength);
+    // InsertSort(Massive, MassLength);
+
+    // Метод тредоемкости (n)^1.2
     // ShellSort(Massive, MassLength);
+
+    // Быстрые методы сортировки
+    HeapSort(Massive, MassLength);
     PrintMas(Massive, MassLength);
 }
---
+++ src/lib/HeapSort.h
@@ -0,0 +1,8 @@
+#ifndef HEAP_SORT_H_
+#define HEAP_SORT_H_
+
+#include "ArrayProcessingMethods.h"
+
+void HeapSort(int * Array, int ArrayLength);
+
+#endif  // HEAP_SORT_H_
